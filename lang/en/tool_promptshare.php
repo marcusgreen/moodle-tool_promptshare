703314ba345f0ff7a171e361f3fe2fd8d1b93dfd
Marcus Green <marcusavgreen@gmail.com>
2026-01-31 20:15:30 +0000

    update to strings

---
+++ lang/en/tool_promptshare.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * English language strings for Promptshare tool
+ *
+ * @package    tool_promptshare
+ * @category   string
+ * @copyright  2026 Marcus Green
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Prompt share';
+$string['promptsettings'] = 'Prompt settings';
+$string['privacy:metadata'] = 'The Prompt share plugin does not store any personal data.';
